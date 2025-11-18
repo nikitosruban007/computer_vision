2d108ef01635c602e6daca8ad43358e249ec0e58
Mykyta Ruban <53084330+nikitosruban007@users.noreply.github.com>
2025-11-18 14:20:24 +0200

    Add files via upload

---
+++ CW_lesson13.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+from tensorflow.keras import layers, models
+import numpy as np
+
+from tensorflow.keras.preprocessing import image
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory('data/train', image_size=(128, 128), batch_size=30, label_mode='categorical')
+test_ds = tf.keras.preprocessing.image_dataset_from_directory('data/test', image_size=(128, 128), batch_size=30, label_mode='categorical')
+
+normalization_layer = layers.Rescaling(1./255)
+train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+test_ds = test_ds.map(lambda x, y: (normalization_layer(x), y))
+
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)))
+
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(3, activation='softmax'))
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(train_ds, epochs=15, validation_data=test_ds, verbose=2)
+
+test_lost, test_accuracy = model.evaluate(test_ds)
+print('Test loss:', test_lost)
+print('Test accuracy:', test_accuracy)
+
+class_name = ['cars', 'cats', 'dogs']
+
+img = image.load_img('data/FILE_NAME', target_size=(128, 128))
+img_array = image.img_to_array(img)
+
+img_array = img_array/255.0
+img_array = np.expand_dims(img_array, axis=0)
+
+predictions = model.predict(img_array)
+predicted_index = np.argmax(predictions[0])
+
+print(f'Імовірність по класам: {predictions[0]}')
+print(f'Модель визначила:  {class_name[predicted_index]}')
